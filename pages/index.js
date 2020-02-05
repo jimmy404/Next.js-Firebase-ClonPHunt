1548f11d918715e90e73e7fbdaaf71d914f1dc89
Jimmy <bruno_pier@hotmail.com>
2020-02-05 20:27:37 -0300

    Deleting Css styles - index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
-import styled from '@emotion/styled';
-
-const Heading = styled.h1`
-color: red`;
 
 const Home = () => (
   <div>
-    <Heading>Inicio</Heading>
+    <h1>Inicio</h1>
   </div>
 )
 
