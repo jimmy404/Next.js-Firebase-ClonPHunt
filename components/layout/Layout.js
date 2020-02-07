82f767ffbbe652c720d4512024f47d4364607bc5
Jimmy <bruno_pier@hotmail.com>
2020-02-06 22:52:39 -0300

    Adding Global import - Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import {Global, css} from '@emotion/core';
 
 const Layout = props => {
     return (
         <>
+        <Global />
             <Header />
 
             <main>
