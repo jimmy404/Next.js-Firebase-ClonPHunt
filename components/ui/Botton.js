f4b0c960ab715161ebb3eef6a3068158d25cc96d
Jimmy <bruno_pier@hotmail.com>
2020-02-06 23:44:31 -0300

    Adding Boton file&styled code - Boton.js

---
+++ components/ui/Botton.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Boton = styled.a`
+    font-weight: 700;
+    text-transform: uppercase;
+    border: 1px solid #d1d1d1;
+    padding: .8rem 2rem;
+    margin-right: 1rem;
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
+
+export default Boton;
