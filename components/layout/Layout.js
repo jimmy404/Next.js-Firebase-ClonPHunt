4d9b230c056f7a96aa36dc2aacd62e8f7159ac15
Jimmy <bruno_pier@hotmail.com>
2020-02-06 22:51:52 -0300

    Deleting import Link - Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
 import Header from './Header';
 
 const Layout = props => {
     return (
         <>
             <Header />
-
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">nosotros</Link>
-            </nav>
 
             <main>
                 {props.children}
