cb8aa4d2e740a75143e278bb0d757fb977965db8
Jimmy <bruno_pier@hotmail.com>
2020-02-06 23:34:45 -0300

    Adding wrap&ContenedorHeader - Header.js

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,6 +5,16 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
+const ContenedorHeader = styled.div`
+    max-width: 1200;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width:768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
 const Header = () => {
     return (
         <header
@@ -13,7 +23,7 @@ const Header = () => {
                 padding: 1rem 0;
             `}
         >
-            <div>
+        <ContenedorHeader>
                 <div>
                     <p>P</p>
 
@@ -27,7 +37,7 @@ const Header = () => {
                             <Link href="/">Login</Link>
                             <Link href="/">Crear Cuenta</Link>
                     </div>
-            </div>
+        </ContenedorHeader>
         </header>
     );
 }
