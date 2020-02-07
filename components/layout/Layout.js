8a434c7a583e4e0e5970d251f984faeb112be68d
Jimmy <bruno_pier@hotmail.com>
2020-02-06 22:57:50 -0300

    Adding inline styles - Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,7 +5,38 @@ import {Global, css} from '@emotion/core';
 const Layout = props => {
     return (
         <>
-        <Global />
+        <Global
+            styles={css`
+                :root {
+                    --gris: #3d3d3d;
+                    --gris2: #6F6F6F;
+                    --naranja: #DA55dF
+                }
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box;
+                }
+                *, *:before, *:after {
+                    box-sizing: inherit;
+                }
+                body {
+                    font-size: 1.6rem;
+                    line-height: 1.5;
+                }
+                h1, h2, h3 {
+                    margin: 0 0 2rem 0;
+                    line-height: 1.5;
+                }
+                ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                }
+                a {
+                    text-decoration: none;
+                }
+            `}
+        />
             <Header />
 
             <main>
