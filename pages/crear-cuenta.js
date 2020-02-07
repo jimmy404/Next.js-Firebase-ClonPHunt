ff4c9ded8f3114a23f7059dcd0efed540c7f0a14
Jimmy <bruno_pier@hotmail.com>
2020-02-07 19:16:16 -0300

    Adding form code - crear-cuenta.js

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -4,7 +4,29 @@ import Layout from '../components/layout/Layout';
 const CrearCuenta = () => (
     <div>
         <Layout>
-            <h1>Crear Cuenta</h1>
+            <>
+                <h1>Crear Cuenta</h1>
+                <form>
+                    <div>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            placeholder="Tu Nombre"
+                            name="nombre"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Tu Email"
+                            name="email"
+                        />
+                    </div>
+                </form>
+            </>
         </Layout>
     </div>
 )
