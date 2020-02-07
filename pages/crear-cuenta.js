3424bda9e312795aaf7a854001456b42763720ce
Jimmy <bruno_pier@hotmail.com>
2020-02-07 20:33:16 -0300

    Adding STATE_INICIAL & return code - crear-cuenta.js

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -4,52 +4,67 @@ import Layout from '../components/layout/Layout';
 import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario';
 
 
-const CrearCuenta = () => (
-    <div>
-        <Layout>
-            <>
-                <h1
-                    css={css`
-                        text-align: center;
-                        margin-top: 5rem;
-                    `}
-                >Crear Cuenta</h1>
-                <Formulario>
-                    <Campo>
-                        <label htmlFor="nombre">Nombre</label>
-                        <input
-                            type="text"
-                            id="nombre"
-                            placeholder="Tu Nombre"
-                            name="nombre"
-                        />
-                    </Campo>
-                    <Campo>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            placeholder="Tu Email"
-                            name="email"
-                        />
-                    </Campo>
-                    <Campo>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Tu Password"
-                            name="password"
-                        />
-                    </Campo>
-                    <InputSubmit
-                        type="submit"
-                        value="Crear Cuenta"
-                    />
-                </Formulario>
-            </>
-        </Layout>
-    </div>
-)
+//Validaciones
+import useValidacion from '../hooks/useValidacion';
+
+const STATE_INICIAL = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+
+const CrearCuenta = () => {
+
+    const {} = useValidacion(STATE_INICIAL)
+
+    return (
+            <div>
+                <Layout>
+                    <>
+                        <h1
+                            css={css`
+                                text-align: center;
+                                margin-top: 5rem;
+                            `}
+                        >Crear Cuenta</h1>
+                        <Formulario>
+                            <Campo>
+                                <label htmlFor="nombre">Nombre</label>
+                                <input
+                                    type="text"
+                                    id="nombre"
+                                    placeholder="Tu Nombre"
+                                    name="nombre"
+                                />
+                            </Campo>
+                            <Campo>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="Tu Email"
+                                    name="email"
+                                />
+                            </Campo>
+                            <Campo>
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    placeholder="Tu Password"
+                                    name="password"
+                                />
+                            </Campo>
+                            <InputSubmit
+                                type="submit"
+                                value="Crear Cuenta"
+                            />
+                        </Formulario>
+                    </>
+                </Layout>
+            </div>
+    )
+}
+
 
 export default CrearCuenta
