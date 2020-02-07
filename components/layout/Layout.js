62622f9f0fe8abfb1ac80a48b44f0c76b0e4cefe
Jimmy <bruno_pier@hotmail.com>
2020-02-06 23:02:46 -0300

    Adding Normalize - Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -38,6 +38,18 @@ const Layout = props => {
                 }
             `}
         />
+
+                <Head>
+                    <html lang="es" />
+                    <title>Proyecto F&N</title>
+                    <link
+                        rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+                        integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+                        crossorigin="anonymous"
+                    />
+                </Head>
+
             <Header />
 
             <main>
