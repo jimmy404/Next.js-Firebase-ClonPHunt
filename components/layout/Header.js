bbb331c14e5da160586cb6b26b32df775161aa2d
Jimmy <bruno_pier@hotmail.com>
2020-02-06 22:38:38 -0300

    Adding Header file&code

---
+++ components/layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <div>
+                    <p>P</p>
+
+                    {/* Buscador */}
+                    {/* Navegador */}
+
+                </div>
+                    <div>
+                    {/* Buscador */}
+                    </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
