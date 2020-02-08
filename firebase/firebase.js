9a7ca52f84b943fc37be02ab5d4c38c8e4319150
Jimmy <bruno_pier@hotmail.com>
2020-02-08 19:02:30 -0300

    Adding code to cerrarSesion - firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -22,6 +22,10 @@ class Firebase {
     async login(email, password){
         return this.auth.signInWithEmailAndPassword(email, password);
     }
+    //cierra la sesion del usuario
+    async cerrarSesion(){
+        await this.auth.signOut();
+    }
 }
 
 const firebase = new Firebase();
