ec064395a73f28c9b4d26de49f4c4929ef3e6d57
Jimmy <bruno_pier@hotmail.com>
2020-02-08 16:51:29 -0300

    Adding data proyect from firebase

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAnQp5aPaczfTgavRvrQ32ECsaW_B62EBM",
+    authDomain: "product-hunt-e6c42.firebaseapp.com",
+    databaseURL: "https://product-hunt-e6c42.firebaseio.com",
+    projectId: "product-hunt-e6c42",
+    storageBucket: "product-hunt-e6c42.appspot.com",
+    messagingSenderId: "712907109035",
+    appId: "1:712907109035:web:b24dc4414d1002e52b3dee"
+};
