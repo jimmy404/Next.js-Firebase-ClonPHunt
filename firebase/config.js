bb4c1da2dcb3b7f41534ca1820700b299da87813
Jimmy <bruno_pier@hotmail.com>
2020-02-08 16:51:58 -0300

    Adding export - config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAnQp5aPaczfTgavRvrQ32ECsaW_B62EBM",
     authDomain: "product-hunt-e6c42.firebaseapp.com",
     databaseURL: "https://product-hunt-e6c42.firebaseio.com",
@@ -7,3 +7,5 @@ var firebaseConfig = {
     messagingSenderId: "712907109035",
     appId: "1:712907109035:web:b24dc4414d1002e52b3dee"
 };
+
+export default firebaseConfig;
