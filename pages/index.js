740c0e92fcf2c723a459f61dc95adb43b5120332
Jimmy <bruno_pier@hotmail.com>
2020-02-09 13:06:10 -0300

    Adding return in Home - index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout/Layout';
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Inicio</h1>
-    </Layout>
-  </div>
-)
+
+const Home = () => {
+
+  return (
+      <div>
+        <Layout>
+          <h1>Inicio</h1>
+        </Layout>
+      </div>
+  )
+}
 
 export default Home
